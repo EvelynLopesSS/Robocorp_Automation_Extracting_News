a09405e89c207ceaa013941723db1d5fb317dfaf
Evelyn Lopes <evelynlopesjob@gmail.com>
2024-09-02 23:43:50 -0300

    upload workitems

--- tasks.py
+++ tasks.py
@@ -1,6 +1,21 @@
 from robocorp.tasks import task
+from robocorp.workitems import WorkItems
+
 from main import main
+
 
 @task
 def news_scrapper():
-    main("brazil", "economy", 2)
+    """
+    This function is the main entry point for the Robocorp Task.
+    It retrieves the country and category from the Work Item variables,
+    and then calls the main function with these values and the number of pages to scrape (2).
+    """
+    # Get the Work Item variables
+    wi = WorkItems()
+    country = wi.get_variable("country")
+    category = wi.get_variable("category")
+    months = wi.get_variable("months")
+
+    main(country, category, months)
+    #main("brazil", "economy", 2)
